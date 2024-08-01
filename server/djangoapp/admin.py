77cb9139660059dc2a6ff69b0513434aee2cf93e
OrnelieManz <ornelie.manz@gmail.com>
2024-08-01 13:16:53 -0400

    updating codes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,7 +11,7 @@ class CarModelInline(admin.StackedInline):
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    fields = ['car_make','name', 'type', 'year', 'dealer']
+    fields = ['car_make','name', 'type', 'year']
     
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -32,7 +32,7 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
-    dealer = models.IntegerField()
+    #dealer = models.IntegerField()
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
