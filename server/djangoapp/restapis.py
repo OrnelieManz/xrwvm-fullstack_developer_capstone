7ce0c87d11320ba8ca814482086f40eee8351ea0
OrnelieManz <ornelie.manz@gmail.com>
2024-08-02 10:55:33 -0400

    updating codes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,6 +2,7 @@
 # import requests
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -25,9 +26,9 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Network exception occurred: {e}")
     finally:
         print("GET request call complete!")
 
@@ -48,6 +49,6 @@ def post_review(data_dict):
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Network exception occurred : {e}")
 # Add code for posting review
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,7 +1,8 @@
 import LoginPanel from "./components/Login/Login"
-import SubmitPanel from "./components/Register/Register"
-import { Routes, Route } from "react-router-dom";
+import SubmitPanel from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<SubmitPanel />}/>
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
