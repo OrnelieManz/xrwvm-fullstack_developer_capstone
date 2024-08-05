b6a7c41b3d610385e6ed987a872c1dfb9f1e1990
Ornelie Manzambi <57666977+OrnelieManz@users.noreply.github.com>
2024-08-05 17:47:04 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -14,7 +14,7 @@ class CarMake(models.Model):
     description = models.TextField()
     
     # Other fields as needed
-    def __str__(self): 
+    def __str__(self):
         
         return self.name  # Return the name as the string representation
 
@@ -29,8 +29,8 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, 
-    on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake,
+        on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -40,13 +40,13 @@ class CarModel(models.Model):
         ('MINIVAN', 'Minivan'),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=30, 
-    choices=CAR_TYPES, default='SUV')
+    type = models.CharField(max_length=30,
+        choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2024,
-        validators=[
-        MaxValueValidator(2024),
-            MinValueValidator(2015)
-        ])
+            validators=[
+                MaxValueValidator(2024),
+                MinValueValidator(2015)
+            ])
     
     def __str__(self):
         return self.name  # Return the name as the string representation
