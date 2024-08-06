e445b26dd5e890776177541f43d84111cec16658
OrnelieManz <ornelie.manz@gmail.com>
2024-08-06 06:11:08 -0400

    updating codes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://orneliemanz-8000.theiadockernext-1-'
+                 'https://orneliemanz-8000.theiadockernext-0-'
                  'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://orneliemanz-8000.theiadockernext-1-'
+CSRF_TRUSTED_ORIGINS = ['https://orneliemanz-8000.theiadockernext-0-'
                         'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
